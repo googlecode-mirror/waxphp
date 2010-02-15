f803a56c93e3699880d083642d15aec5be030838
joechrz <joechrz@deab1398-4293-11de-b77d-cbd14124f061>
2010-02-15 23:31:59 +0000

    Removed support for Role properties, cleaned up DCIObject, more work towards handling ambiguity

--- core/include/DCI/DCIObject.php
+++ core/include/DCI/DCIObject.php
@@ -1,51 +1,28 @@
 <?php
 	/**
-	* The DCIObject is any object that can perform roles
-	*
-	* This class is responsible for performing the background work
-	* required for having role-playing objects, which includes
-	* reflection, static context redirection, and determining if
-	* a role or role action can be played/performed
-	*/
-	
-	/**
-	* The base DCIObject class
-	*
 	* This class provides base functionality for the rest of the framework.
 	* The DCIObject class is meant to be extended and as a result provides
 	* the ability to implement role methods, properties, and performs all 
 	* necessary redirection to fully implement DCI
 	*
 	* @author Joe Chrzanowski <joechrz@gmail.com>
-	* @version 2.0
+	* @version 3.0
 	*/
 	class DCIObject {
 		/**
-        * Shortcut to non-ambiguous methods
+        * array to hold the parent role for all injected methods
         */
 		var $methods = array();
 		
 		/**
-		* Shortcuts to non-ambiguous properties
-		*/
-		var $properties = array();
-		
-		/**
-		* List of all roles and their methods and properties
+		* Reverse index of methods -- stores a list of roles
+		* and their respective methods.
 		*/
 		var $roles = array();
 		
 		/**
 		* the $fallback variable solves problems with conflicting methods and
-		* properties by defining a default role to use.  Every time a new role
-		* is added, this variable is updated.  Note that this especially useful
-		* in the case of an _inline_context (defined in /wax/core/include/lib.php):
-		*
-		* _inline_context(function (rSpecificRole $myobj) use ($myobj) {
-		*     // from here, $myobj's fallback role is rSpecificRole
-		*     // after this function has executed, the fallback is switched
-		*     // back to its previous value   
-		* });
+		* properties by defining a default role to use in the case of ambiguity.
 		*/
 		var $fallback = '';
 
@@ -66,107 +43,65 @@
         }
 		
 		/**
-		* While it is possible to add a role after initialization,
-		* the type hinting required by some roles causes errors
+		* While it is technically possible to add a role after initialization,
+		* the type hinting used in role and context methods can only happen at
+		* compile-time.
 		*/
 		private function addRole($role) {
 		    $roleclass = $role . "Actions";
-			
 			if (interface_exists($role) && class_exists($roleclass)) {
 			    $this->roles[$role] = array();
-			    $this->roles[$role]['properties'] = array();
 			    $this->roles[$role]['methods'] = array();
-			    			    
-    			foreach (get_class_vars($roleclass) as $property => $value) {
-    			    $this->roles[$role]['properties'][$property] = $value;
-    			    
-    			    if (array_key_exists($property,$this->properties))
-    			        $this->properties[$property][] = $role;
-    			    else
-    			        $this->properties[$property] = array($role);
-    			}
-			
     			foreach (get_class_methods($roleclass) as $method) {
     			    $this->roles[$role]['methods'][$method] = true;
-    			    
     			    if (array_key_exists($method,$this->methods))
     			        $this->methods[$method][] = $role;
     			    else
     			        $this->methods[$method] = array($role);
     			}
-    			
     			$this->fallback = $role;
     		}
     		else throw new RoleNotFoundException($role);
 		}
 		
+		/**
+		* Explicitly specify both the role and method name.  Essentially equivalent to:
+		* rRoleNameActions::method($this, ... )
+		*
+		* Calling an injected method eventually gets routed to this function.
+		*/
 		function call_explicit($role, $method, $args) {
 		    array_unshift($args, $this);
 		    $roleclass = $role . "Actions";
-		    
-		    if (class_exists($roleclass) && method_exists($roleclass,$method)) {
+		    if (class_exists($roleclass) && method_exists($roleclass,$method))
                 return call_user_func_array(array($roleclass,$method),$args);
-            }
-            else {
+            else 
                 throw new MethodNotFoundException("$role::$method",$this->roles[$role]);
-            }
 		}
 		
-		function call_default($method, $args) {
-		    if (isset($this->methods[$method])) {
-		        return $this->call_explicit(end($this->methods[$method]), $method, $args);
-		    }
-		}
-		
-		function call($func, $args) {
-		    if (isset($this->methods[$func]) && count($this->methods[$func]) > 1) {
-		        return $this->call_explicit($this->fallback, $func, $args);
+		/**
+		* Call a role method:
+		*   First, check for ambiguity.  If unambiguous, call method
+		*   If ambiguous, see if the fallback role contains the method
+		*   If not, call the most recently declared method and throw a notice
+		*   If all else fails, MethodNotFoundException
+		*/
+        function __call($func, $args) {
+            if (isset($this->methods[$func]) && count($this->methods[$func]) > 1) {
+                foreach ($this->methods[$func] as $role) {
+                    if ($role == $this->fallback) 
+                        return $this->call_explicit($this->fallback, $func, $args);
+                }
+                
+    		    trigger_error("<b>Ambiguous Method</b>: $func in $trace[file]:$trace[line]" . 
+    		        "<br /><pre>" . print_r($this->methods[$func],true) . "</pre>");
+    		        
+    		    return $this->call_explicit(end($this->methods[$func]), $func, $args);
 		    }
 		    else if (isset($this->methods[$func]) && is_array($this->methods[$func]))
-		        return $this->call_explicit($this->methods[$func][0], $func, $args);
+		        return $this->call_explicit(end($this->methods[$func]), $func, $args);
 		    else 
 		        throw new MethodNotFoundException("::$func",array_keys($this->roles));
-		}
-
-		// TODO: Try to determine calling role -- 
-		// restrict property access to their creating roles
-		function __get($var) {
-		    if (array_key_exists($var,$this->properties)) {
-		        if (isset($this->properties[$var]) && count($this->properties[$var]) > 1) {
-		            throw new AmbiguousPropertyException($var,$this->properties[$var]);
-		        }
-		        else {
-		            return $this->roles[$this->properties[$var][0]]['properties'][$var];
-		        }
-		    }
-		    else {
-		        throw new PropertyNotFoundException($var, $this->properties);
-		    }
-		}
-		function __set($var,$value) {
-		    if (array_key_exists($var,$this->properties)) {
-		        if (isset($this->properties[$var]) && count($this->properties[$var]) > 1) {
-		            throw new AmbiguousPropertyException($var,$this->properties[$var]);
-		        }
-		        else {
-		            return $this->roles[$this->properties[$var][0]]['properties'][$var] = $value;
-		        }
-		    }
-		    else {
-		        throw new PropertyNotFoundException($var, $this->properties);
-		    }
-		}
-        function __call($func, $args) {
-            try {
-                return $this->call($func,$args);
-            }
-            catch (AmbiguousMethodException $ame) {
-                // throw a PHP notice to alert the programmer to the ambiguity.
-                // they can silence the error by turning off error reporting or
-                // using the shutup (@) operator
-                trigger_error("<b>Ambiguous Method</b> $func in: " . implode(", ",$this->methods[$func]));
-                return $this->call_default($func,$args);
-            }
         }
 		
 	}
